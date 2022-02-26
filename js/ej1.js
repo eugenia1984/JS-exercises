67373a5efb6386e0a348ce01f524bc5b3191c630
Eugenia <costamariaeugenia1@gmail.com>
2022-02-26 16:52:55 -0300

    Update ej1.js

--- js/ej1.js
+++ js/ej1.js
@@ -2,9 +2,12 @@
 //Por ejemplo: miFunción("Hola Mundo") devolverá 10.
 
 
-// Creo la función expresada contarCadena (la guardo en varaible) que primero va a fijarse si la cadena está vacía y lo avisa, sino informa que cadena se ingreso y cuántos caracteres tiene. La escribo como arrow function
+// Creo la función expresada contarCadena y la guardo en variable
+// primero va a fijarse si la cadena está vacía y lo avisa
+// sino informa que cadena se ingreso y cuántos caracteres tiene. 
+// La escribo como arrow function
 const contarCaracteres = (cadena ="")=>(!cadena)?console.log("No ingresaste ninguna cadena"):console.log(`La cadena "${cadena}" tiene ${cadena.length} caracteres.`);
 
-// Con console.log llamo a la función contarCaracteres y la apaso por parámetro la string "Hola mundo" que se ve por consola
+// Con console.log llamo a la función contarCaracteres y le paso por parámetro la string "Hola mundo" que se ve por consola
 console.log(contarCaracteres("Hola mundo"));
 
