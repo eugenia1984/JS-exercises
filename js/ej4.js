7d5e916d00b44e2304776f4c2b0962d4c8990c42
Eugenia <costamariaeugenia1@gmail.com>
2022-02-26 17:47:26 -0300

    Update ej4.js

--- js/ej4.js
+++ js/ej4.js
@@ -2,17 +2,14 @@
 
 //Creo la función repetirTexto que recibe los parámetros texto (lo declaro como cadena vacía) y veces (lo declaro como undefined)
 //Ahora voy a poner todos los if que vayan validando los posibles errores, para que sea más expresivo va sin llaves y todo en la misma línea
-//Primer validación: que la cadena no venga vacía, lee el if y finaliza en el return con un console.warn.
-//Segunda validación: si no ingresa el número de veces a repetir el texto
-//Tercera validación: que el número de veces a repetir el texto no sea 0
-//Cuarta validación: que el número de veces a repetir el texto no sea un número negativo
 const repetirTexto = (texto = "",veces=undefined)=>{
+  //Primer validación: que la cadena no venga vacía, lee el if y finaliza en el return con un console.warn.
   if(!texto) return console.warn("No ingresaste un texto");
-
+  //Segunda validación: si no ingresa el número de veces a repetir el texto
   if(veces === undefined) return console.warn("No ingresaste el número de veces a repetir el texto");
-  
+  //Tercera validación: que el número de veces a repetir el texto no sea 0
   if(veces = 0) return console.error("El número de veces no puede ser 0");
-
+  //Cuarta validación: que el número de veces a repetir el texto no sea un número negativo
   if(matchMedia.toString(veces)===-1) return console.error("El número de veces no puede ser negativo");
   
   for(let i=1;i<=veces;i++)console.info(`${texto},${i}`);
